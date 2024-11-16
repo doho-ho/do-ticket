7857d97b9b1f5418be33dac180b70838c5a7a39b
도호 <edh1021@gmail.com>
2024-11-16 16:03:34 +0900

    init

---
+++ src/main/kotlin/doho/doticket/DoticketApplication.kt
@@ -0,0 +1,11 @@
+package doho.doticket
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DoticketApplication
+
+fun main(args: Array<String>) {
+    runApplication<DoticketApplication>(*args)
+}
---
+++ src/test/kotlin/doho/doticket/DoticketApplicationTests.kt
@@ -0,0 +1,13 @@
+package doho.doticket
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DoticketApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
